9f387d4118fa9c3f122aa80a24a7005030a5c6f9
Silva Filho <tarusilva@gmail.com>
2022-08-09 19:57:00 -0300

    Criação da pasta database e do arquivo db.sql com  schema do banco de dados.

---
+++ classe-01/database/db.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE biblioteca;
+
+DROP TABLE IF EXISTS autores;
+
+CREATE TABLE autores (
+    id SERIAL PRIMARY KEY,
+    nome TEXT NOT NULL,
+    idade SMALLINT
+);
+
+DROP TABLE IF EXISTS livros;
+
+CREATE TABLE livros (
+    id SERIAL PRIMARY KEY,
+    id_autor INTEGER NOT NULL REFERENCES autores(id),
+    nome TEXT NOT NULL,
+    editora VARCHAR(100),
+    genero VARCHAR(50) NOT NULL,
+    data_publicacao DATE
+    /* FOREIGN KEY (id_autor) REFERENCES autores(id) */
+);
+
+DROP TABLE IF EXISTS usuarios;
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    idade SMALLINT,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    telefone CHAR(11),
+    cpf CHAR(11) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS emprestimos;
+
+CREATE TABLE emprestimos (
+    id SERIAL PRIMARY KEY,
+    id_usuario INTEGER NOT NULL REFERENCES usuarios(id),
+    id_livro INTEGER NOT NULL REFERENCES livros(id),
+    -- o CHAR(9) faz surgir um espaço vazio no final do 'pendente' 
+    -- para poder usar os 9 espaços determinados;
+    -- talvez trocar para VARCHAR(9) resolva.
+    status CHAR(9) DEFAULT 'pendente'
+);
---
+++ classe-01/database/query.sql
@@ -0,0 +1,26 @@
+INSERT INTO autores(nome, idade) 
+VALUES 
+('Napoleon Hill', 45),
+('Robert C. Martin', 50),
+('Martin Fowler', 55),
+('Erich Gamma', 60);
+
+INSERT INTO livros(id_autor, nome, editora, genero, data_publicacao)
+VALUES 
+(1, 'Quem pensa enriquece', 'Citadel', 'Autoajuda', '2020-11-01'),
+(1, 'Mais esperto que o Diabo', 'Citadel', 'Autoajuda', '2014-07-10'),
+(2, 'Código limpo', 'Alta Books', 'Desenvolvimento de software', '2008-09-08'),
+(2, 'Arquitetura limpa', 'Alta Books', 'Desenvolvimento de software', '2019-04-19'),
+(3, 'Refatoração', 'Novatec', 'Desenvolvimento de software', '2020-04-30'),
+(4, 'Padrões de Projetos', 'Bookman', 'Desenvolvimento de software', '2000-01-01');
+
+INSERT INTO usuarios(nome, idade, email, telefone, cpf) 
+VALUES 
+('Aretha Montgomery', 30, 'augue.id.ante@odioAliquam.com', '83301247289', '80371350042'),
+('Camden H. Bartlett', 15, 'turpis.vitae.purus@risusDuisa.ca', '07351620035', '67642869061'),
+('Raja W. Coffey Thomas', 30, 'raja.feugiat@nonummy.com', '05326108887', '63193310034'),
+('Elton D. Olsen', 29, 'auctor@duiFuscediam.edu', '66367637073', '75670505018'),
+('Elmo K. Greer', 18, 'risus.Duis@eget.ca', '12382472542', '82539841031');
+
+INSERT INTO emprestimos(id_usuario, id_livro) 
+VALUES (1, 2), (2, 1), (3, 3), (3, 5), (4, 6), (5, 4), (3, 6);
