9744b55de7ebe7109db23c1b53e72a440a2771bd
Silva Filho <tarusilva@gmail.com>
2022-08-10 17:11:17 -0300

    Atualização da conexão com o banco de dados através das variáveis env.

--- src/conexao.js
+++ src/conexao.js
@@ -1,20 +1,17 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    // user: "postgres",
     user: process.env.DB_USER,
-    host: "localhost",
-    // database: "biblioteca",
-    database: process.env.DB_DATABSE,
-    // password: "Hn#1zTj&",
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: 5432
 });
 
 const query = (text, param) => {
     return pool.query(text, param);
-}
+};
 
 module.exports = {
     query
-}
+};
