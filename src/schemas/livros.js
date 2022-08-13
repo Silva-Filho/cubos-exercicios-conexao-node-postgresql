cf903feceb8ef4c9cf894c90387b5c40d04360be
Silva Filho <tarusilva@gmail.com>
2022-08-13 20:31:34 -0300

    Criação do schema do Yup para validar os dados que serão atualizados de um livro.

--- src/schemas/livros.js
+++ src/schemas/livros.js
@@ -8,6 +8,15 @@ const schemaCadastroLivro = yup.object().shape({
     data_publicacao: yup.date(),
 });
 
+const schemaAtualizacaoLivro = yup.object().shape({
+    id_autor: yup.number().strict(), 
+    nome: yup.string().strict(), 
+    genero: yup.string().strict(), 
+    editora: yup.string().strict(), 
+    data_publicacao: yup.date(),
+});
+
 module.exports = {
     schemaCadastroLivro,
+    schemaAtualizacaoLivro,
 };
