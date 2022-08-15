30009404563eb449195246463e134873726b5ece
Silva Filho <tarusilva@gmail.com>
2022-08-15 16:17:02 -0300

    Criação do schema Yup para validar os dados do usuário a serem atualizados.

--- src/schemas/usuarios.js
+++ src/schemas/usuarios.js
@@ -4,10 +4,19 @@ const schemaCadastroUsuario = yup.object().shape({
     nome: yup.string().required(), 
     idade: yup.number().strict(), 
     email: yup.string().email().required(), 
-    telefone: yup.string(), 
+    telefone: yup.string().strict(), 
     cpf: yup.number().strict().required(),
+});
+
+const schemaAtualizacaoUsuario = yup.object().shape({
+    nome: yup.string(), 
+    idade: yup.number().strict(), 
+    email: yup.string().email(), 
+    telefone: yup.string().strict(), 
+    cpf: yup.number().strict(),
 });
 
 module.exports = {
     schemaCadastroUsuario,
+    schemaAtualizacaoUsuario,
 };
