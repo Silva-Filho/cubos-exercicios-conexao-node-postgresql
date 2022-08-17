40bec47f954f5320b28be602eb23fa1e1c90ff7c
Silva Filho <tarusilva@gmail.com>
2022-08-16 22:32:04 -0300

    Criação do middleware verificarEmprestimoExiste.

---
+++ src/middlewares/emprestimos.js
@@ -0,0 +1,37 @@
+const conexao = require("../conexao");
+
+const verificarEmprestimoExiste = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const query = `
+            select 
+                e.id as id_emprestimo, 
+                u.nome as nome_usuario, 
+                u.telefone, 
+                u.email, 
+                l.nome as nome_livro, 
+                e.status_emprestimo 
+            from emprestimos e
+            left join usuarios u on e.id_usuario = u.id 
+            left join livros l on e.id_livro = l.id 
+            where e.id = $1
+        `;
+
+        const { rows: emprestimo } = await conexao.query(query, [id]);
+
+        if (emprestimo.length === 0) {
+            return res.status(404).json("Empréstimo não encontrado.");
+        }
+
+        req.emprestimo = emprestimo[0];
+
+        next();
+    } catch (error) {
+        return res.status(400).json(error.message);
+    } 
+};
+
+module.exports = {
+    verificarEmprestimoExiste,
+};
