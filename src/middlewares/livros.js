e63329e4beb55872156c6dbc3f06a1f881a35b0f
Silva Filho <tarusilva@gmail.com>
2022-09-07 21:38:25 -0300

    refactor: reestruturação da lógica do middleware verificarLivroExiste.
    Criação de verificação em acordo com a url:
    - /livros
    - /emprestimos
    
    Nelas foi usada a função utilitária encontrarLivro.

--- src/middlewares/livros.js
+++ src/middlewares/livros.js
@@ -1,75 +1,77 @@
-const conexao = require("../conexao");
-const { 
-    schemaCadastroLivro, 
-    schemaAtualizacaoLivro 
-} = require("../schemas/livros");
+const conexao = require( "../conexao" );
+const { encontrarLivro } = require( "../utils/livros" );
+const {
+    schemaCadastroLivro,
+    schemaAtualizacaoLivro
+} = require( "../schemas/livros" );
 
-const verificarLivroExiste = async (req, res, next) => {
+const verificarLivroExiste = async ( req, res, next ) => {
     try {
-        const { id } = req.params;
+        const url = req.url;
 
-        const query = `
-            select 
-                l.id as id_livro, 
-                l.nome as nome_livro, 
-                a.nome as nome_autor, 
-                l.genero, 
-                l.editora, 
-                l.data_publicacao 
-            from livros l 
-            left join autores a on l.id_autor = a.id 
-            where l.id = $1
-        `;
+        if ( url.includes( "/livros" ) ) {
+            const { id } = req.params;
 
-        const { rows: livro } = await conexao.query(query, [id]);
+            req.livro = await encontrarLivro( res, id );
 
-        if (livro.length === 0) {
-            return res.status(404).json(`O id params ${id} informado não corresponde a nenhum livro cadastrado.`);
+            if ( !req.livro[ 0 ] ) {
+                return;
+            }
         }
-        
-        req.livro = livro[0];
 
-        next();
-    } catch (error) {
-        return res.status(400).json(error.message);
-    } 
-};
+        if ( url.includes( "/emprestimos" ) ) {
+            const { id_livro } = req.body;
 
-const verificarDadosCadastroLivro = async (req, res, next) => {
-    try {
-        await schemaCadastroLivro.validate(req.body);
+            if ( id_livro ) {
+                req.livro = await encontrarLivro( res, id_livro );
 
-        next();
-    } catch (error) {
-        return res.status(400).json(error.message);
-    }
-    
-};
-
-const verificarDadosAtualizacaoLivro = async (req, res, next) => {
-    try {
-        await schemaAtualizacaoLivro.validate(req.body);
-
-        next();
-    } catch (error) {
-        return res.status(400).json(error.message);
-    }
-    
-};
-
-const verificarLivroPossuiEmprestimo = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-
-        const existeEmprestimos = await conexao.query("select * from emprestimos where id_livro = $1", [id]);
-
-        if (existeEmprestimos.rowCount > 0) {
-            return res.status(400).json("Não é possível excluir um livro que possui empréstimos.");
+                if ( !req.livro[ 0 ] ) {
+                    return;
+                }
+            }
         }
 
         next();
-    } catch (error) {
-        return res.status(400).json(error.message);
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
+    }
+};
+
+const verificarDadosCadastroLivro = async ( req, res, next ) => {
+    try {
+        await schemaCadastroLivro.validate( req.body );
+
+        next();
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
+    }
+
+};
+
+const verificarDadosAtualizacaoLivro = async ( req, res, next ) => {
+    try {
+        await schemaAtualizacaoLivro.validate( req.body );
+
+        next();
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
+    }
+
+};
+
+const verificarLivroPossuiEmprestimo = async ( req, res, next ) => {
+    try {
+        const { id } = req.params;
+
+        const existeEmprestimos = await conexao.query( "select * from emprestimos where id_livro = $1", [ id ] );
+
+        if ( existeEmprestimos.rowCount > 0 ) {
+            return res.status( 400 ).json( "Não é possível excluir um livro que possui empréstimos." );
+        }
+
+        next();
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
     }
 };
 
