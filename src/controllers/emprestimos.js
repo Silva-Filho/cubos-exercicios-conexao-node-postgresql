fb4d4cf1e08df3e3df3820b4c8583efda46a826e
Silva Filho <tarusilva@gmail.com>
2022-09-08 19:18:56 -0300

    refactor: Reestruturado o controller cadastrarEmprestimo.
    Remoção das verificações para os respectivos middlewares:
    
    - verificarDadosCadastroEmprestimo, para verificar se as informações
    passadas no corpo da requisição estão de acordo com o
    schemaCadastroEmprestimo;
    
    - verificarUsuarioExiste, para verificar se o id do usuário informado
     no corpo da requisição existe no banco de dados; e
    
    - verificarLivroExiste, para verificar se o id do livro informado
    no corpo da requisição existe no banco de dados.

--- src/controllers/emprestimos.js
+++ src/controllers/emprestimos.js
@@ -1,6 +1,6 @@
-const conexao = require("../conexao");
+const conexao = require( "../conexao" );
 
-const listarEmprestimos = async (req, res) => {
+const listarEmprestimos = async ( req, res ) => {
     try {
         const query = `
             select 
@@ -15,121 +15,105 @@ const listarEmprestimos = async (req, res) => {
             left join livros l on e.id_livro = l.id
         `;
 
-        const { rows: emprestimos } = await conexao.query(query);
+        const { rows: emprestimos } = await conexao.query( query );
 
-        return res.status(200).json(emprestimos);
-    } catch (error) {
-        return res.status(400).json(error.message);
+        return res.status( 200 ).json( emprestimos );
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
     }
 };
 
-const obterEmprestimo = async (req, res) => {
+const obterEmprestimo = async ( req, res ) => {
     try {
         const { emprestimo } = req;
 
-        return res.status(200).json(emprestimo);
-    } catch (error) {
-        return res.status(400).json(error.message);
+        return res.status( 200 ).json( emprestimo );
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
     }
 };
 
-const cadastrarEmprestimo = async (req, res) => {
-    const { id_usuario, id_livro } = req.body;
-
+const cadastrarEmprestimo = async ( req, res ) => {
     try {
-        if ( !id_usuario || !id_livro ) {
-            return res.status(400).json("Os campos id_usuario e id_livro são obrigatórios.");
-        }
-
-        const usuario = await conexao.query("select * from usuarios where id = $1", [id_usuario]);
-
-        if (usuario.rowCount === 0) {
-            return res.status(404).json("Usuario não encontrado.");
-        }
-
-        const livro = await conexao.query("select * from livros where id = $1", [id_livro]);
-
-        if (livro.rowCount === 0) {
-            return res.status(404).json("Livro não encontrado.");
-        }
+        const { id_usuario, id_livro } = req.body;
 
         const query = "insert into emprestimos (id_usuario, id_livro) values ($1, $2)";
-        const emprestimo = await conexao.query(query, [id_usuario, id_livro]);
+        const emprestimo = await conexao.query( query, [ id_usuario, id_livro ] );
 
-        if (emprestimo.rowCount === 0) {
-            return res.status(400).json("Não foi possível cadastrar o empréstimo.");
+        if ( emprestimo.rowCount === 0 ) {
+            return res.status( 400 ).json( "Não foi possível cadastrar o empréstimo." );
         }
 
-        return res.status(200).json("Empréstimo cadastrado com sucesso.");
-    } catch (error) {
-        return res.status(400).json(error.message);
+        return res.status( 200 ).json( "Empréstimo cadastrado com sucesso." );
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
     }
 };
 
-const atualizarEmprestimo = async (req, res) => {
+const atualizarEmprestimo = async ( req, res ) => {
     const { id } = req.params;
     const { status } = req.body;
 
     try {
-        if (!status) {
-            return res.status(400).json("O campo status é obrigatório.");
-        }
-        
-        if (status !== "pendente" && status !== "devolvido") {
-            return res.status(400).json("Status informado incompatível com 'pendente' ou 'devolvido'.");
+        if ( !status ) {
+            return res.status( 400 ).json( "O campo status é obrigatório." );
         }
 
-        const emprestimo = await conexao.query("select * from emprestimos where id = $1", [id]);
+        if ( status !== "pendente" && status !== "devolvido" ) {
+            return res.status( 400 ).json( "Status informado incompatível com 'pendente' ou 'devolvido'." );
+        }
 
-        if (emprestimo.rowCount === 0) {
-            return res.status(404).json("Empréstimo não encontrado.");
+        const emprestimo = await conexao.query( "select * from emprestimos where id = $1", [ id ] );
+
+        if ( emprestimo.rowCount === 0 ) {
+            return res.status( 404 ).json( "Empréstimo não encontrado." );
         }
 
         const query = "update emprestimos set status = $1 where id = $2";
-        const emprestimoAtualizado = await conexao.query(query, [status, id]);
+        const emprestimoAtualizado = await conexao.query( query, [ status, id ] );
 
-        if (emprestimoAtualizado.rowCount === 0) {
-            return res.status(404).json("Não foi possível atualizar o empréstimo.");
+        if ( emprestimoAtualizado.rowCount === 0 ) {
+            return res.status( 404 ).json( "Não foi possível atualizar o empréstimo." );
         }
 
-        return res.status(200).json("Empréstimo foi atualizado com sucesso.");
-    } catch (error) {
-        return res.status(400).json(error.message);
+        return res.status( 200 ).json( "Empréstimo foi atualizado com sucesso." );
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
     }
 };
 
-const excluirEmprestimo = async (req, res) => {
+const excluirEmprestimo = async ( req, res ) => {
     const { id } = req.params;
 
     try {
-        const emprestimo = await conexao.query("select * from emprestimos where id = $1", [id]);
+        const emprestimo = await conexao.query( "select * from emprestimos where id = $1", [ id ] );
 
-        if (emprestimo.rowCount === 0) {
-            return res.status(404).json("Empréstimo não encontrado.");
+        if ( emprestimo.rowCount === 0 ) {
+            return res.status( 404 ).json( "Empréstimo não encontrado." );
         }
 
         // @ts-ignore
-        if (emprestimo.rows[0].status.trim() === "pendente") {
-            return res.status(404).json("Não é possível excluir empréstimo com status igual a pendente.");
+        if ( emprestimo.rows[ 0 ].status.trim() === "pendente" ) {
+            return res.status( 404 ).json( "Não é possível excluir empréstimo com status igual a pendente." );
         }
 
         const query = "delete from emprestimos where id = $1";
-        const emprestimoExcluido = await conexao.query(query, [id]);
+        const emprestimoExcluido = await conexao.query( query, [ id ] );
 
-        if (emprestimoExcluido.rowCount === 0) {
-            return res.status(404).json("Não foi possível excluir o empréstimo.");
+        if ( emprestimoExcluido.rowCount === 0 ) {
+            return res.status( 404 ).json( "Não foi possível excluir o empréstimo." );
         }
 
-        return res.status(200).json("Empréstimo foi excluído com sucesso.");
-    } catch (error) {
-        return res.status(400).json(error.message);
+        return res.status( 200 ).json( "Empréstimo foi excluído com sucesso." );
+    } catch ( error ) {
+        return res.status( 400 ).json( error.message );
     }
 };
 
 module.exports = {
     listarEmprestimos,
-    obterEmprestimo, 
-    cadastrarEmprestimo, 
+    obterEmprestimo,
+    cadastrarEmprestimo,
     atualizarEmprestimo,
     excluirEmprestimo
 };
